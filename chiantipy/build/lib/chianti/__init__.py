c8b9eb563ae3fefe109aa4f7288f46d8d9a6e029
Ken Dere <kdere@gmu.edu>
2015-01-16 17:20:07 +0000

    creating new trunk from newgui

---
+++ chiantipy/build/lib/chianti/__init__.py
@@ -0,0 +1,71 @@
+'''
+the ChiantiPy - CHIANTI Python package
+calculates various aspects of emission line and continua from the
+CHIANTI atomic database for astrophysical spectroscopy
+'''
+from . import  version
+Version = version.__version__
+Version_info = version.__version_info__
+#
+#from __future__ import absolute_import
+#import os
+#import constants
+#import filters
+#import mputil
+#from . import constants
+#from . import filters
+#from . import mputil
+#
+#try:
+#    chInteractive = int(os.environ['CHIANTIPY_INTERACTIVE'])
+#except:
+#    chInteractive = 1
+
+#if chInteractive:
+#    import pylab as pl
+#else:
+#    import matplotlib
+#    matplotlib.use('Agg')
+#    import matplotlib.pyplot as pl
+###
+#xuvtop = os.environ['XUVTOP']
+##chInteractive=1
+#Defaults = util.defaultsRead(verbose = chInteractive)
+#Ip = util.ipRead()
+#MasterList = util.masterListRead()
+#AbundanceAll = util.abundanceRead(abundancename = Defaults['abundfile'])
+#IoneqAll = util.ioneqRead(ioneqname = Defaults['ioneqfile'])
+#import version
+#__version__ = version.__version__
+#__version_info__ = version.__version_info__
+#import core
+#import pylab as pl
+#if pl.rcParams['backend'].lower() == 'qt4agg':
+#    # these imports are not compatible with python3
+#    from gui_qt.gui import gui
+#elif pl.rcParams['backend'].lower() == 'wxagg':
+#    from gui_wx.gui import gui
+#elif pl.rcParams['backend'].lower() == 'gtkagg':
+#    from gui_cl.gui import gui
+#elif pl.rcParams['backend'].lower() == 'agg':
+#    from gui_cl.gui import gui
+#elif pl.rcParams['backend'].lower() == 'agg':
+#    from gui_cl.gui import gui
+#elif pl.rcParams['backend'].lower() == 'macosx':
+#    from  gui_cl.gui import gui
+#else:
+#    print((' - Warning - \n' \
+#          + ' - in order to use the various gui dialogs, the matlpotlib/pylab backend needs \n' \
+#          +' - to be either Qt4Agg or WXAgg - \n' \
+#         +' - in order to use the command line dialogs, the matlpotlib/pylab backend needs \n' \
+#         +' - to be GTKAgg or MacOSX - \n' \
+#        + ' - current backend is %s \n '%(pl.rcParams['backend'])))
+#    print((' - the full functionality of the chianti.core.ion class may not be available \n'))
+#    print((' - it would probably be better to set your matplotlib backend to either \n'))
+#    print((' - Qt4Agg, WXAgg, GTKAgg, or MacOSX \n'))
+#    print((' - using the command line dialogs for now but there could be problems - \n'))
+#    import gui_cl.gui as gui
+##
+## placed here because util needs gui
+#from . import util
+
---
+++ chiantipy/build/lib/chianti/data.py
@@ -0,0 +1,41 @@
+'''
+module for collecting various top-level Chianti data
+for the keyword arguments below
+temperature = temperature in Kelvin
+eDensity is the electron density per cubic cm
+hDensity is the hydrogen density per cubic cm
+pDensity is the proton density per cubic cm
+radTemperature is the radiation temperature of central source
+rStar is the distance of the plasma from the source in units of the sources radius
+distance is the distance from the central source
+'''
+import os, fnmatch
+from  . import util
+
+###
+xuvtop = os.environ['XUVTOP']
+#chInteractive=1
+Defaults = util.defaultsRead()
+Ip = util.ipRead()
+MasterList = util.masterListRead()
+#AbundanceAll = util.abundanceRead(abundancename = Defaults['abundfile'])
+IoneqAll = util.ioneqRead(ioneqname = Defaults['ioneqfile'])
+# gets the ChianitPy version
+# gets the version of the CHIANTI database
+ChiantiVersion = util.versionRead()
+keywordArgs = ['temperature','eDensity','hDensity', 'pDensity','radTemperature', 'rStar', 'distance']
+#
+abunddir = os.path.join(xuvtop,'abundance')
+filelist = os.listdir(abunddir)
+#
+abundList = []
+for one in filelist:
+    fname = os.path.join(abunddir,one)
+    if os.path.isfile(fname):
+        abundList.append(os.path.splitext(one)[0])
+#for one in abundList:
+#    print(one)
+Abundance = {abundList[0]:util.abundanceRead(abundancename = abundList[0])}
+for one in abundList[1:]:
+    Abundance[one] = util.abundanceRead(abundancename = one)
+
